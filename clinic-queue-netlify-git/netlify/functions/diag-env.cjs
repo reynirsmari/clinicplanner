47b450eb36ef44f541a2a46fbb10f098a4aff861
reynirsmari <reynir.smari@gmail.com>
2025-08-16 22:48:25 +0000

    Delete clinic-queue-netlify-git/netlify/functions directory

--- clinic-queue-netlify-git/netlify/functions/diag-env.cjs
+++
@@ -1,9 +0,0 @@
-// Reports presence of env vars (true/false) for debugging.
-exports.handler = async () => {
-  const names = [
-    'BLOBS_SITE_ID','NETLIFY_SITE_ID','SITE_ID',
-    'BLOBS_TOKEN','NETLIFY_API_TOKEN','NETLIFY_TOKEN'
-  ];
-  const present = Object.fromEntries(names.map(n => [n, !!process.env[n]]));
-  return { statusCode: 200, headers: { 'content-type': 'application/json' }, body: JSON.stringify(present) };
-};
