de43185fa19a657cce9bc45311d651cd5954eeef
reynirsmari <reynir.smari@gmail.com>
2025-08-16 22:10:36 +0000

    Update tickets-list.cjs

--- clinic-queue-netlify-git/netlify/functions/tickets-list.cjs
+++ clinic-queue-netlify-git/netlify/functions/tickets-list.cjs
@@ -1,5 +1,5 @@
+// netlify/functions/tickets-list.cjs
 const { readAll, recalc, json } = require('./storage.cjs');
-
 exports.handler = async function () {
   const all = await readAll();
   recalc(all);
