41375cd2e52f640a34650c3d08c7df65b46f3366
reynirsmari <reynir.smari@gmail.com>
2025-08-17 08:14:41 +0000

    Delete clinic-queue-netlify-git/netlify/functions directory

--- clinic-queue-netlify-git/netlify/functions/whoami.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-exports.handler = async () => {
-  const dir = __dirname;
-  let files = [];
-  try { files = fs.readdirSync(dir); } catch {}
-  return { statusCode: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ dir, files, marker: 'manual-auth-canary' }) };
-};
