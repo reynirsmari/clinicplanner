6625f8efeec5ce21a4ad7bb2866a6edb9a698214
reynirsmari <reynir.smari@gmail.com>
2025-08-17 08:14:19 +0000

    Add files via upload

--- clinic-queue-netlify-git/netlify/functions/health.js
+++ clinic-queue-netlify-git/netlify/functions/health.js
@@ -1,6 +1,5 @@
-const { envSeen } = require('./storage.cjs');
 exports.handler = async () => ({
   statusCode: 200,
   headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ seen: envSeen() })
+  body: JSON.stringify({ ok: true, ts: Date.now() })
 });
