e5f7229352638f89a9c7b8b0bc9464f794db7b43
reynirsmari <reynir.smari@gmail.com>
2025-08-17 08:15:57 +0000

    Delete clinic-queue-netlify-git/netlify 7/functions directory

--- clinic-queue-netlify-git/netlify 7/functions/env-check.js
+++
@@ -1,6 +0,0 @@
-const { envSeen } = require('./storage.cjs');
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ seen: envSeen() })
-});
