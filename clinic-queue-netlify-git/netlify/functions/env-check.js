16cef1f52120dbe82e324d44b3203dcd910d65a7
reynirsmari <reynir.smari@gmail.com>
2025-08-21 15:05:04 +0000

    Update env-check.js

--- clinic-queue-netlify-git/netlify/functions/env-check.js
+++ clinic-queue-netlify-git/netlify/functions/env-check.js
@@ -1,17 +1,16 @@
-
-export async function handler() {
+module.exports.handler = async () => {
   const seen = {
-    BLOBS_SITE_ID: !!process.env.BLOBS_SITE_ID,
+    BLOBS_SITE_ID: !!(process.env.BLOBS_SITE_ID || process.env.NETLIFY_SITE_ID || process.env.SITE_ID),
     NETLIFY_SITE_ID: !!process.env.NETLIFY_SITE_ID,
     SITE_ID: !!process.env.SITE_ID,
     BLOBS_TOKEN: !!process.env.BLOBS_TOKEN,
     NETLIFY_API_TOKEN: !!process.env.NETLIFY_API_TOKEN,
     NETLIFY_TOKEN: !!process.env.NETLIFY_TOKEN,
-    BLOBS_STORE: process.env.BLOBS_STORE || 'queue'
+    BLOBS_STORE: process.env.BLOBS_STORE || 'queue',
   };
   return {
     statusCode: 200,
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({ seen }),
   };
-}
+};
