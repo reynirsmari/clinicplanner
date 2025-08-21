85bfe26828b88c2a2489e7f96d53564a65873d9f
reynirsmari <reynir.smari@gmail.com>
2025-08-21 14:25:43 +0000

    Delete clinic-queue-netlify-git/netlify/functions directory

--- clinic-queue-netlify-git/netlify/functions/_shared/json.js
+++
@@ -1,7 +0,0 @@
-export function json(status, bodyObj) {
-  return {
-    statusCode: status,
-    headers: { "content-type": "application/json; charset=utf-8", "cache-control": "no-store" },
-    body: JSON.stringify(bodyObj),
-  };
-}
--- clinic-queue-netlify-git/netlify/functions/whoami.js
+++
@@ -1,9 +0,0 @@
-import { json } from "./_shared/json.js";
-import fs from "node:fs";
-import path from "node:path";
-export const handler = async () => {
-  const dir = path.dirname(new URL(import.meta.url).pathname);
-  let files = [];
-  try { files = fs.readdirSync(dir).filter(f => f.endsWith(".js")); } catch {}
-  return json(200, { dir, files, marker: "manual-auth-canary" });
-};
