282adf5f06bf213bf7e73913a0eca153074743c2
reynirsmari <reynir.smari@gmail.com>
2025-08-21 15:05:17 +0000

    Update whoami.js

--- clinic-queue-netlify-git/netlify/functions/whoami.js
+++ clinic-queue-netlify-git/netlify/functions/whoami.js
@@ -1,8 +1,7 @@
-
-export async function handler() {
+module.exports.handler = async () => {
   return {
     statusCode: 200,
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({ cwd: process.cwd(), ts: Date.now() }),
   };
-}
+};
